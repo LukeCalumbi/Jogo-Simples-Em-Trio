128594e6459586bc2b0b8cbab61092c022508abf
ThiLizard <thiagomma.lizard@gmail.com>
2024-04-18 16:57:20 -0300

    MenuPrincipal

---
+++ JoguinhoEmTrio/Assets/Scripts/Menu/MenuPrincipal.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPrincipal : MonoBehaviour
+{
+    [SerializeField] private string nomeDoLevelDeJoge;
+    [SerializeField] private GameObject painelMenuInicial;
+    [SerializeField] private GameObject painelCreditos;
+    public void jogar()
+    {
+        SceneManager.LoadScene("Jogo");
+    }
+
+    public void AbrirCreditos()
+    {
+        painelMenuInicial.SetActive(false);
+        painelCreditos.SetActive(true);
+    }
+
+    public void FecharCreditos()
+    {
+        painelCreditos.SetActive(false);
+        painelMenuInicial.SetActive(true);
+
+    }
+
+    public void SairJogo()
+    {
+        Debug.Log("Sair do Jogo");
+        Application.Quit();
+    }
+}
